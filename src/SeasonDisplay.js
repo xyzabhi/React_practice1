a40b23dd2a0719cff3e4b5b8ace05a43784b287c
xyzabhi <abhinavkumar8083@gmail.com>
2021-03-01 17:23:31 +0530

    styling added

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,17 @@
 import React from "react";
+import "./SeasonDisplay.css";
 import "semantic-ui-css/semantic.min.css";
 
+const seasonConfig = {
+  summer: {
+    text: "Lets hit the beach!",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burr it is cold!",
+    iconName: "snowflake",
+  },
+};
 const getSeason = (lat, month) => {
   if (month + 1 > 2 && month + 1 < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -11,15 +22,12 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const weather_data =
-    season === "winter"
-      ? { text: "Burr,Its too chilly", icon: "snowflake" }
-      : { text: "It hot, Lets hit the Beach", icon: "sun" };
+  const { text, iconName } = seasonConfig[season];
   return (
-    <div>
-      <i className={`${weather_data.icon} icon`}></i>
-      <h1>{weather_data.text}</h1>
-      <i className={`${weather_data.icon} icon`}></i>
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left ${iconName} icon massive`}></i>
+      <h1>{text}</h1>
+      <i className={`icon-right ${iconName} icon massive`}></i>
     </div>
   );
 };
