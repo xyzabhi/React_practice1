d6f8317bef1db404891ce5f61cb7e50560b11a12
xyzabhi <abhinavkumar8083@gmail.com>
2021-03-01 17:30:13 +0530

    Loading spinner is added

---
+++ src/Spinner.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Spiner = () => {
+  return (
+    <div className="ui active dimmer">
+      <div className="ui text loader">Loading..</div>
+    </div>
+  );
+};
+export default Spiner;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spiner from "./Spinner";
 
 class App extends React.Component {
   state = { lat: null, errMessage: "" };
@@ -19,7 +20,7 @@ class App extends React.Component {
     }
     if (!this.state.errMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />;
-    } else return <div>Loading!</div>;
+    } else return <Spiner />;
   }
 }
 
